179ce75976309ed914368c5db8bebc407522b98f
Fabriano28 <ferumbromin@gmail.com>
2022-07-27 14:28:07 +0700

    Initial Commit

---
+++ MyLearningSpace/MyLearningSpaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyLearningSpaceApp.swift
+//  MyLearningSpace
+//
+//  Created by Farrel Brian on 27/07/22.
+//
+
+import SwiftUI
+
+@main
+struct MyLearningSpaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
